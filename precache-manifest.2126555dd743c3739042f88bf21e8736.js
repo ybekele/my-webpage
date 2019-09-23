ace83b86e6bcb1ba6fb3415e8246fd07ae2bc01f
ybekele <yonael@ualberta.ca>
2019-09-23 09:15:44 -0600

    Updates

---
+++ precache-manifest.2126555dd743c3739042f88bf21e8736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089de7bb4e803a271f78b376870e09f1",
+    "url": "/my-webpage/index.html"
+  },
+  {
+    "revision": "72e86470e1653b489783",
+    "url": "/my-webpage/static/css/main.374391ae.chunk.css"
+  },
+  {
+    "revision": "0c1b6079bffc9cd4d7b4",
+    "url": "/my-webpage/static/js/2.a06aba72.chunk.js"
+  },
+  {
+    "revision": "72e86470e1653b489783",
+    "url": "/my-webpage/static/js/main.c5e73bb5.chunk.js"
+  },
+  {
+    "revision": "6c8ef103ac683631421d",
+    "url": "/my-webpage/static/js/runtime~main.deaf9aea.js"
+  }
+]);
